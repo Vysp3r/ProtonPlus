6818a3defeef7efcbadcfafe98ef706875dc492b
Arcitec <38923130+Arcitec@users.noreply.github.com>
2024-09-09 02:03:36 +0200

    Added new scripts for building native app and rebuilding translations

---
+++ scripts/build-native.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${SCRIPT_DIR}/.."
+
+meson build --wipe --prefix=/usr
+cd build
+ninja
