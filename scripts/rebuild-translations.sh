89c5223fd3a12a0f3fdb8c19cf256bfafb7af9b0
Arcitec <38923130+Arcitec@users.noreply.github.com>
2024-09-12 20:42:20 +0200

    Enhance translation rebuild scripts to use new build directory

--- scripts/rebuild-translations.sh
+++ scripts/rebuild-translations.sh
@@ -6,8 +6,9 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}/.."
 
 # We must first perform a native build.
-meson build --wipe --prefix=/usr
-cd build
+BUILD_DIR="build-native"
+meson "${BUILD_DIR}" --wipe --prefix=/usr
+cd "${BUILD_DIR}"
 ninja
 
 # Now we can update the translations.
