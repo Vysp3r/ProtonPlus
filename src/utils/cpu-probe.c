e2e06fb06c52786eab005a96afdf267f6da31a1b
Xarblu <xarblu@protonmail.com>
2026-08-14 12:00:02 +0200

    Make ProtonPlusCpuFeatureProbe an opaque type
    valac seems to redefine this:
    
      src/libprotonplus_core.a.p/utils/system.c:206:8: error: redefinition of '_ProtonPlusCpuFeatureProbe'
        206 | struct _ProtonPlusCpuFeatureProbe {
            |        ^
      ../ProtonPlus-0.6.4/src/utils/cpu-probe.h:5:16: note: previous definition is here
          5 | typedef struct _ProtonPlusCpuFeatureProbe {
            |                ^
    
    Signed-off-by: Xarblu <xarblu@protonmail.com>

--- src/utils/cpu-probe.c
+++ src/utils/cpu-probe.c
@@ -10,6 +10,39 @@
 #if (defined(__x86_64__) || defined(_M_X64)) && (defined(__GNUC__) || defined(__clang__))
 #include <cpuid.h>
 
+struct _ProtonPlusCpuFeatureProbe {
+    gboolean available;
+    gboolean cmpxchg16b;
+    gboolean lahf_sahf;
+    gboolean popcnt;
+    gboolean sse3;
+    gboolean sse4_1;
+    gboolean sse4_2;
+    gboolean ssse3;
+    gboolean avx;
+    gboolean avx2;
+    gboolean bmi1;
+    gboolean bmi2;
+    gboolean f16c;
+    gboolean fma;
+    gboolean lzcnt;
+    gboolean movbe;
+    gboolean osxsave;
+    gboolean xcr0_xmm;
+    gboolean xcr0_ymm;
+    gboolean avx512f;
+    gboolean avx512bw;
+    gboolean avx512cd;
+    gboolean avx512dq;
+    gboolean avx512vl;
+    gboolean xcr0_opmask;
+    gboolean xcr0_zmm_hi256;
+    gboolean xcr0_hi16_zmm;
+    gboolean aarch64_crc32;
+    gboolean aarch64_lse_atomics;
+    gboolean aarch64_rdma;
+};
+
 static uint64_t
 read_xcr0 (void)
 {
